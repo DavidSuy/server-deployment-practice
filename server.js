8da21cb0489df81b95a847d71a500e420a0eee2d
David Suy <DavidSuyDev@gmail.com>
2022-09-19 16:11:28 -0700

    add .envSample

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello Word');
