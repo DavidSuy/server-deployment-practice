000fe8f366f3583ccfecf1064f56ad5419de08f7
David Suy <DavidSuyDev@gmail.com>
2022-09-19 17:34:29 -0700

    fix testing bug

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send('Hello Word');
 });
+
 app.get('/greeting', (req, res) => {
   res.json({
     greeting: 'Hello World',
   });
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
 });
 
 module.exports = app;
