0eb6c0b96a6be880a699007a8c5df3f3a66b5bd0
David Suy <DavidSuyDev@gmail.com>
2022-10-10 17:27:22 -0700

    add eb workflow

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello Word');
+  res.send('<h1>Hello world!!!</h1>');
+});
+
+app.get('/test', (req, res) => {
+  res.json('<h1>test</h1>');
 });
 
 app.get('/greeting', (req, res) => {
